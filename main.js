14b9365b6a37b6853e6e6a44daa2de48dce21290
ehsan <s.baloot2@gmail.com>
2017-06-05 07:00:50 +0400

    توضیح تبدیل رشته های پیکسلی به عدد

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ function number_to_px(a){
     var c=b.concat("px"); //خیلی راحت می شد مقدار px رو بهش اضافه کرد
     return c;
 }
-function px_to_number(p){
-    p=p.substring(0,p.length-2);
-    var n=Number(p);
-    return n;
+function px_to_number(p){  //ابتدا رشته ی p را بگیر
+    p=p.substring(0,p.length-2);//دو حرف آخر این رشته را حذف کن یعنی مقدار px را حذف کن
+    var n=Number(p); //این رشته رو به عدد تبدیل کن
+    return n; //حالا عدد رو برگردان
 
 }
 //**************************ok buttons********************************************************
